7c115973a5c14b06beab3dfe7fbf4b0b320f020e
Mostafa Kamal <=>
2020-09-25 22:35:28 +0600

    Updates

---
+++ precache-manifest.b46eed6010e92352ea2938461d683dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8015f76bae707f270b695c3ec9c30030",
+    "url": "/expense-track/index.html"
+  },
+  {
+    "revision": "1b1c8f2a07f1615b859d",
+    "url": "/expense-track/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bde08870dcff5bb54c6f",
+    "url": "/expense-track/static/js/2.81c11ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-track/static/js/2.81c11ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1c8f2a07f1615b859d",
+    "url": "/expense-track/static/js/main.0cd70f3d.chunk.js"
+  },
+  {
+    "revision": "ac9db4a0304f4e694915",
+    "url": "/expense-track/static/js/runtime-main.95b63f57.js"
+  }
+]);
